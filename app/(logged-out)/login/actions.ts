afea54e96c1a77be789dc695032847cc0d3680a7
navi1213 <timemans3@gmail.com>
2024-12-04 01:02:11 +0900

    マージコンフリクトを解決

--- app/(logged-out)/login/actions.ts
+++ app/(logged-out)/login/actions.ts
@@ -1,14 +1,6 @@
 "use server";
 
-<<<<<<< HEAD
 import { z } from "zod";
-=======
-import db from "@/db/drizzle";
-import { passwordMatchSchema } from "@/validation/passwordMatchSchema";
-import { z } from "zod";
-import { hash } from "bcryptjs";
-import { users } from "@/db/schema";
->>>>>>> 03bf1a321dad9fe1d55e22029f12345d06738c11
 import { passwordSchema } from "@/validation/passwordSchema";
 import { signIn } from "@/auth";
 export const loginWithCredential = async ({
@@ -19,11 +11,7 @@ export const loginWithCredential = async ({
   password: string;
 }) => {
   const loginSchema = z.object({
-<<<<<<< HEAD
     email: z.string().email("正しい形式のメールアドレスを入力してください"),
-=======
-    email: z.string().email(),
->>>>>>> 03bf1a321dad9fe1d55e22029f12345d06738c11
     password: passwordSchema,
   });
   // safeParse は、データを検証し、期待通りのデータであればそのデータを返します。そうでなければ、エラーメッセージを出力します。
--- db/schema.ts
+++ db/schema.ts
@@ -1,5 +1,2 @@
 export { users } from "./usersSchema";
-<<<<<<< HEAD
 export { passwordResetTokens } from "./passwordResetTokensSchema";
-=======
->>>>>>> 03bf1a321dad9fe1d55e22029f12345d06738c11
