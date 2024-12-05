d1c084a9c0dda34fa751ecb732a56ef63eb60a26
navi1213 <timemans3@gmail.com>
2024-12-05 15:44:37 +0900

    メタデータを変更.faviconも変更

---
+++ config/site.ts
@@ -0,0 +1,12 @@
+import { SiteConfig } from "@/types";
+
+export const siteConfig:SiteConfig = {
+    name:"散歩シェア",
+    description:"散歩のコースをシェアできるサイトです。",
+    url:"http://localhost:3000",
+    ogImage:"",
+    links:{
+        x:"https://x.com/ikea01234",
+        github:"https://github.com/navi1213"
+    }
+}
