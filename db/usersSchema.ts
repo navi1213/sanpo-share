8f56335a4854b93d9c1cc6daee3ef457e847e0ab
navi1213 <timemans3@gmail.com>
2024-12-23 00:11:31 +0900

    vercelエラーに対処2

--- db/usersSchema.ts
+++ db/usersSchema.ts
@@ -4,7 +4,7 @@ export const users = pgTable("users", {
   id: serial("id").primaryKey(),
   username: text("user_name").unique().notNull(), // user_nameに一意性制約を追加
   email: text("email").unique(),
-  password: text("password"),
+  password: text("password").notNull(),
   createdAt: timestamp("created_at").defaultNow(),
   twoFactorSecret: text("2fa_secret"),
   twoFactorActivated: boolean("2fa_activated").default(false),
